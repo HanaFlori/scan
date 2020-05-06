b169c540660c46f768862a9b721ef0b842f8b61d
HanaFlori <48555939+HanaFlori@users.noreply.github.com>
2020-05-06 20:11:07 +0300

    Add files via upload

---
+++ precache-manifest.8d394081c5e23f52d70497999ea5eed8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ab08871434e4307909f896d28170e9",
+    "url": "/scan/index.html"
+  },
+  {
+    "revision": "f8484483ffd942028844",
+    "url": "/scan/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7dc7bca8209be68aee8d",
+    "url": "/scan/static/css/main.d2317254.chunk.css"
+  },
+  {
+    "revision": "f8484483ffd942028844",
+    "url": "/scan/static/js/2.6fe21b3f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/scan/static/js/2.6fe21b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc7bca8209be68aee8d",
+    "url": "/scan/static/js/main.f500d051.chunk.js"
+  },
+  {
+    "revision": "8fead94c779ca21a7bc6",
+    "url": "/scan/static/js/runtime-main.fdb9a23b.js"
+  }
+]);
